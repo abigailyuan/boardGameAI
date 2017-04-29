966dea82925db90a74b08b473fa124dcb024aea5
abigailyuan <abigail_yuan@hotmail.com>
2017-04-29 19:41:13 +1000

    add Oliver and Abby to the game

---
+++ PartB/src/myuan3/Oliver.java
@@ -0,0 +1,51 @@
+package myuan3;
+
+import aiproj.slider.SliderPlayer;
+
+import java.util.Scanner;
+
+import aiproj.slider.Move;
+
+public class Oliver implements SliderPlayer{
+	
+	Board myBoard;
+	char playerType;
+	
+	public Oliver() {
+		
+	}
+
+	@Override
+	public void init(int dimension, String board, char player) {
+		
+		//initialise a board with dimention
+		myBoard = new Board(dimension);
+		int row = 0;
+		
+		//read the board
+		Scanner scanner = new Scanner(board);
+		while(scanner.hasNextLine()) {
+			String s = scanner.nextLine();
+			this.myBoard.readRow(row, s);
+			this.myBoard.printRow(row);
+			row++;
+		}
+		scanner.close();
+		
+		//initialise player type
+		this.playerType = player;
+	}
+
+	@Override
+	public void update(Move move) {
+		
+		
+	}
+
+	@Override
+	public Move move() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
